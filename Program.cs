491756bdeb177d397b3e3caeccf5b37b68672ab1
Lizz <golubl692@gmail.com>
2023-12-01 20:14:56 +0100

    new branch

--- Program.cs
+++ Program.cs
@@ -10,6 +10,24 @@ namespace Snake
 {
     class Program
     {
+        // Metoda do rysowania ramki wokół planszy
+        private static void DrawBorder(int screenwidth, int screenheight)
+        {
+            var horizontalBar = string.Join("", new byte[screenwidth].Select(b => "■").ToArray());
+
+            Console.SetCursorPosition(0, 0);
+            Console.Write(horizontalBar);
+            Console.SetCursorPosition(0, screenheight - 1);
+            Console.Write(horizontalBar);
+
+            for (int i = 0; i < screenheight; i++)
+            {
+                Console.SetCursorPosition(0, i);
+                Console.Write("■");
+                Console.SetCursorPosition(screenwidth - 1, i);
+                Console.Write("■");
+            }
+        }
 
         class pixel
         {
@@ -23,9 +41,9 @@ namespace Snake
             int score = 5;
             int gameover = 0;
 
-            //windowsize
-            Console.WindowHeight = 18;
-            Console.WindowWidth = 34;
+            
+            Console.WindowHeight = 16;
+            Console.WindowWidth = 32;
             int screenwidth = Console.WindowWidth;
             int screenheight = Console.WindowHeight;
             Random randomnummer = new Random();
@@ -42,12 +60,71 @@ namespace Snake
             DateTime tijd2 = DateTime.Now;
             string buttonpressed = "no";
 
+            // Narysowanie ramki wokół planszy
+            DrawBorder(screenwidth, screenheight);
+
+                 // Pętla obsługująca ruch węża
+                 while (true)
+                {
+                    tijd2 = DateTime.Now;
+                    if (tijd2.Subtract(tijd).TotalMilliseconds > 500) { break; }
+                    if (Console.KeyAvailable)
+                    {
+                        ConsoleKeyInfo toets = Console.ReadKey(true);
+                        if (toets.Key.Equals(ConsoleKey.UpArrow) && movement != "DOWN" && buttonpressed == "no")
+                        {
+                            movement = "UP";
+                            buttonpressed = "yes";
+                        }
+                        if (toets.Key.Equals(ConsoleKey.DownArrow) && movement != "UP" && buttonpressed == "no")
+                        {
+                            movement = "DOWN";
+                            buttonpressed = "yes";
+                        }
+                        if (toets.Key.Equals(ConsoleKey.LeftArrow) && movement != "RIGHT" && buttonpressed == "no")
+                        {
+                            movement = "LEFT";
+                            buttonpressed = "yes";
+                        }
+                        if (toets.Key.Equals(ConsoleKey.RightArrow) && movement != "LEFT" && buttonpressed == "no")
+                        {
+                            movement = "RIGHT";
+                            buttonpressed = "yes";
+                        }
+                    }
+                }
+                // Dodanie pozycji głowy węża do listy pozycji ciała
+                xposlijf.Add(hoofd.xpos);
+                yposlijf.Add(hoofd.ypos);
+                // Aktualizacja pozycji głowy węża na podstawie wybranego kierunku
+                switch (movement)
+                {
+                    case "UP":
+                        hoofd.ypos--;
+                        break;
+                    case "DOWN":
+                        hoofd.ypos++;
+                        break;
+                    case "LEFT":
+                        hoofd.xpos--;
+                        break;
+                    case "RIGHT":
+                        hoofd.xpos++;
+                        break;
+                }
+
+
+
+
 
 
             Console.SetCursorPosition(screenwidth / 5, screenheight / 2);
             Console.WriteLine("Game over, Score: " + score);
             Console.SetCursorPosition(screenwidth / 5, screenheight / 2 + 1);
         }
+        
+
+
 
     }
 
